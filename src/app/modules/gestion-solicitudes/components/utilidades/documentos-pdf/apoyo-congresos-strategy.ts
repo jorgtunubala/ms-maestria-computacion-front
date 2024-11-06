0ef267c2c37eb2b8cac70b1549d95092cf9d6835
Christian Tobar <70769281+Christian-Tobar@users.noreply.github.com>
2024-11-06 11:01:13 -0500

    ajuste oficio concejo apoyo economico asistencia a evento

--- src/app/modules/gestion-solicitudes/components/utilidades/documentos-pdf/apoyo-congresos-strategy.ts
+++ src/app/modules/gestion-solicitudes/components/utilidades/documentos-pdf/apoyo-congresos-strategy.ts
@@ -123,25 +123,58 @@ export class OficioConcejoApoyoEconomicoCongresos implements DocumentoPDFStrateg
 
         const fechaComite = this.servicioGestor.conceptoComite.fechaAval.split('/');
         const mesEnLetras = this.servicioUtilidades.obtenerMesEnLetras(Number(fechaComite[1]));
-        const { nombreSolicitante, apellidoSolicitante, numeroIdentSolicitante, tipoIdentSolicitante } =
-            this.servicioGestor.infoSolicitud.datosComunSolicitud;
+        const {
+            nombreSolicitante,
+            apellidoSolicitante,
+            numeroIdentSolicitante,
+            tipoIdentSolicitante,
+            codigoSolicitante,
+        } = this.servicioGestor.infoSolicitud.datosComunSolicitud;
 
-        const textAsunto = `Asunto: Solicitud de apoyo económico para estudiante ${nombreSolicitante} ${apellidoSolicitante}\n`;
         const textCuerpo = `${this.servicioGestor.InfoDecano.tratamiento == 'sr.' ? 'Estimado' : 'Estimada'} ${
             this.servicioGestor.InfoDecano.nombreCompleto.split(' ')[0]
-        }, reciba un cordial saludo. Comedidamente me dirijo a usted con el fin de informar que en sesión del día ${
-            fechaComite[0]
-        } de ${mesEnLetras} de ${
+        }, reciba un cordial saludo. Comedidamente me dirijo a usted con el fin de solicitar su colaboración para realizar las gestiones necesarias que permitan conceder apoyo económico solicitado por ${nombreSolicitante.toUpperCase()} ${apellidoSolicitante.toUpperCase()} Cod. ${codigoSolicitante} estudiante del programa de Maestría en Computación, con el fin de participar en el evento de carácter ${
+            this.servicioGestor.infoSolicitud.datosApoyoEconomicoCongreso.tipoCongreso
+        } "${this.servicioGestor.infoSolicitud.datosApoyoEconomicoCongreso.nombreCongreso}" que se llevará a cabo en ${
+            this.servicioGestor.infoSolicitud.datosApoyoEconomicoCongreso.lugarEvento
+        }, del ${this.servicioGestor.infoSolicitud.datosApoyoEconomicoCongreso.fechaInicio} al ${
+            this.servicioGestor.infoSolicitud.datosApoyoEconomicoCongreso.fechaFin
+        }, y donde también se hará la presentación del trabajo titulado "${
+            this.servicioGestor.infoSolicitud.datosApoyoEconomicoCongreso.tituloPublicacion
+        }".\n\nEl comité de programa, en sesión del día ${fechaComite[0]} de ${mesEnLetras} de ${
             fechaComite[2]
-        }, el Comité de Programa avaló la solicitud de apoyo económico para la asistencia al evento: "${
-            this.servicioGestor.infoSolicitud.datosApoyoEconomicoCongreso.nombreCongreso
-        }", presentada por el/la estudiante ${nombreSolicitante.toUpperCase()} ${apellidoSolicitante.toUpperCase()}, con identificación ${tipoIdentSolicitante} ${numeroIdentSolicitante}. Por lo tanto, muy formalmente solicito su colaboración para realizar las gestiones necesarias en este caso.`;
+        }, aprobó el apoyo económico por un valor de COP $${this.servicioUtilidades.numeroAMoneda(
+            this.servicioGestor.infoSolicitud.datosApoyoEconomicoCongreso.valorApoyo
+        )} para cubrir los gastos de desplazamiento y participación en el evento.\n\nSe solicita que el apoyo económico sea consignado directamente en la cuenta bancaria proporcionada.\n`;
+
+        const textDatosApoyo = `Valor apoyo económico: COP $${this.servicioUtilidades.numeroAMoneda(
+            this.servicioGestor.infoSolicitud.datosApoyoEconomicoCongreso.valorApoyo
+        )}\nEntidad Bancaria: ${
+            this.servicioGestor.infoSolicitud.datosApoyoEconomicoCongreso.entidadBancaria
+        }\nTipo de Cuenta: ${
+            this.servicioGestor.infoSolicitud.datosApoyoEconomicoCongreso.tipoCuenta
+        }\nNúmero de Cuenta: ${this.servicioGestor.infoSolicitud.datosApoyoEconomicoCongreso.numeroCuenta}\nTitular: ${
+            this.servicioGestor.infoSolicitud.datosComunSolicitud.nombreSolicitante
+        } ${this.servicioGestor.infoSolicitud.datosComunSolicitud.apellidoSolicitante}\nCédula: ${
+            this.servicioGestor.infoSolicitud.datosComunSolicitud.numeroIdentSolicitante
+        }\nCelular: ${this.servicioGestor.infoSolicitud.datosComunSolicitud.celularSolicitante}\nCorreo Electrónico: ${
+            this.servicioGestor.infoSolicitud.datosComunSolicitud.emailSolicitante
+        }\nDirección de Residencia: ${this.servicioGestor.infoSolicitud.datosApoyoEconomicoCongreso.direccionResidencia}
+        `;
+
         const txtRemitente = `${this.servicioGestor.InfoCoordinador.nombreCompleto.toUpperCase()}\n${
             this.servicioGestor.InfoCoordinador.tratamiento == 'sr.' ? 'Coordinador' : 'Coordinadora'
         } Maestría en Computación`;
 
+        const textAsunto = `Asunto: Solicitud de apoyo económico para asistencia a evento y presentación de artículo de estudiante ${nombreSolicitante} ${apellidoSolicitante}\n`;
+
         let posicionY = this.servicioPDF.agregarContenidoComun(documento, marcaDeAgua, 'consejo');
         posicionY = this.servicioPDF.agregarAsuntoYSolicitud(documento, posicionY, textAsunto, textCuerpo, marcaDeAgua);
+        posicionY = this.servicioPDF.agregarTexto(documento, {
+            text: textDatosApoyo,
+            startY: posicionY,
+            watermark: marcaDeAgua,
+        });
 
         posicionY = this.servicioPDF.agregarDespedida(documento, posicionY + 5, marcaDeAgua);
         posicionY = this.servicioPDF.agregarTexto(documento, {
