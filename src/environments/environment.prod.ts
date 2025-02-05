9c17a6e01322b1ccea615f755c73870a422b9f7f
Christian Tobar <70769281+Christian-Tobar@users.noreply.github.com>
2025-02-05 12:23:57 -0500

    actualizacion de aputnamiento en produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -24,20 +24,20 @@ export const gestion_autenticacion = {
 
 export const gestion_expertos = {
     production: true,
-    api_url: 'https://apptest.unicauca.edu.co:4402/api/',
+    api_url: 'https://apptest.unicauca.edu.co:4414/api/',
 };
 
 export const gestion_egresados = {
     production: true,
-    api_url: 'https://apptest.unicauca.edu.co:4403/api/',
+    api_url: 'https://apptest.unicauca.edu.co:4417/api/',
 };
 
 export const gestion_trabajo_grado = {
     production: true,
-    api_url: 'https://apptest.unicauca.edu.co:4401/api/',
+    api_url: 'https://apptest.unicauca.edu.co:4419/api/',
 };
 
 export const gestion_docentes_estudiantes = {
     production: true,
-    api_url: 'https://apptest.unicauca.edu.co:4402/api/',
+    api_url: 'https://apptest.unicauca.edu.co:4414/api/',
 };
